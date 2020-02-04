04fb943823057394f2101b61bdc0caff17d87a2f
Brayden England <bray.patriot@gmail.com>
2020-02-03 20:51:00 -0700

    Added cat movement, fixed canvas sizing

--- game.js
+++ game.js
@@ -3,11 +3,13 @@ const cvs = document.getElementById('game')
 const c = cvs.getContext('2d')
 
 //canvas height and width
-cvs.width = innerWidth
-cvs.height = innerHeight 
+//cvs.width = innerWidth
+//cvs.height = innerHeight 
+
 
 // * global vars
 let frames = 0
+
 
 // * sprite sheet
 const sprites = new Image()
@@ -17,7 +19,8 @@ sprites.src = './assets/sprites.png'
 let rightPressed = false
 let leftPressed = false
 let spacePressed = false
-let spaceLetGo = true
+let spaceLetGo = true // * why?
+
 document.addEventListener('keydown', function(e){
     //console.log(e.keyCode)
     switch (e.keyCode) {
@@ -38,7 +41,7 @@ document.addEventListener('keydown', function(e){
     }
 })
 document.addEventListener('keyup', function(e){
-    console.log(e.keyCode)
+    //console.log(e.keyCode)
     switch (e.keyCode) {
         case 39: 
         case 68:
@@ -61,34 +64,59 @@ document.addEventListener('keyup', function(e){
 const cat = {
     leftMovement: [{sX: 0, sY: 0}],
     rightMovement: [{sX: 0, sY: 74}],
+    dir: 'right',
     w: 99,
     h: 74,
-    x: innerWidth/2 - (99/2),
-    y: innerHeight - 300,
+    x: cvs.width/2 - (99/2),
+    y: cvs.height - 100,
     dx: 2,
     frame: 0,
 
     // TODO: move control values inside of cat ? maybe
 
     draw: function () {
-        let catFrame = this.rightMovement[this.frame]
+        let catFrame;
+        // choose right or left direction sprites
+        if (this.dir == 'right') {
+            catFrame = this.rightMovement[this.frame]
+        }
+        if (this.dir == 'left') {
+            catFrame = this.leftMovement[this.frame]
+        }
         c.drawImage(sprites, catFrame.sX, catFrame.sY, this.w, this.h, this.x, this.y, this.w, this.h)
 
     },
     update: function () {
-
+        // update cat based on keys
+        let origDir = this.dir
+        if(rightPressed) {
+            this.x += this.dx
+            this.dir = 'right'
+        }
+        if(leftPressed) {
+            this.x -= this.dx
+            this.dir = 'left'
+        }
+        //prevents weird cat turning around thing when both buttons are pressed
+        if(rightPressed && leftPressed) {
+            this.dir = origDir
+        }
     }
 }
 
 // * UPDATE 
 const update = () => {
-
+    cat.update()
 }
 
 // * DRAW
 const draw = () => {
     c.fillStyle = "#936DE8"
     c.fillRect(0,0,cvs.width,cvs.height)
+
+    c.beginPath()
+    c.arc(95, 50, 40, 0, 2 * Math.PI)
+    c.stroke()
 
     cat.draw()
 }
